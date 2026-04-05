1a0309894e0a2c52642241e6ca3db270d62718f5
louiskizos <louisnyembo2@gmail.com>
2026-04-05 21:55:25 +0200

    Fix configuration core-headers

--- api_acedh/settings.py
+++ api_acedh/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['api-acedh.onrender.com', '127.0.0.1', 'localhost'] 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://acedh-site.vercel.app/"
+    "https://acedh-site.vercel.app"
 ]
 
 # Application definition
